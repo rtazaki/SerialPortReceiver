3b9a429ed4d485d4e2a80d90756532f11879d5ba
rtaza <rtazaki@gmail.com>
2023-10-21 20:59:38 +0900

    シリアルポート送受信 rev 0.9

--- SerialPortReceiver/MainWindow.xaml.cs
+++ SerialPortReceiver/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SerialPortReceiver
 {
@@ -20,9 +12,160 @@ namespace SerialPortReceiver
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Serial _serial = new Serial();
         public MainWindow()
         {
             InitializeComponent();
+            // COMポート名の取得
+            ComPort.ItemsSource = _serial.PortNames;
+            ComPort.SelectedIndex = 0;
+            // ヘッダ
+            var header = "00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F \n";
+            ReceiveHeader.Content = header;
+            SendHeader.Content = header;
+            // 送信データ
+            var sendData = Enumerable.Range(0, 256).Select(x => (byte)x);
+            SendData.Text = FormatData(sendData);
+            // 受信イベントの登録
+            _serial.SetReceiveEvent(OnReceived);
+        }
+
+        /// <summary>
+        /// 送信ボタン
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            ComSend.IsEnabled = false;
+            Send();
+        }
+
+        /// <summary>
+        /// 送信処理
+        /// </summary>
+        private void Send()
+        {
+            if (ByteR.IsChecked == true)
+            {
+                var data = SendData.Text.Split()
+                    .SelectMany(x => Encoding.ASCII.GetBytes(x));
+                _serial.Send(data);
+            }
+
+            if (StringR.IsChecked == true)
+            {
+                var data = SendData.Text.Replace("\n", "_");
+                _serial.SendString(data);
+            }
+        }
+
+        /// <summary>
+        /// 受信イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            ReceiveData.Dispatcher.Invoke(
+                new Action(() =>
+                {
+                    var data = string.Empty;
+                    if (ByteR.IsChecked == true)
+                    {
+                        data = FormatData(_serial.Receive());
+                    }
+                    if (StringR.IsChecked == true)
+                    {
+                        data = _serial.ReceiveString().Replace("_", "\n");
+                    }
+                    ReceiveData.Text = data;
+                    // ワンショットの場合、データ送信ボタンのみ活性化
+                    if (OneShot.IsChecked == true)
+                        ComSend.IsEnabled = true;
+                    // 受信契機で送信処理を実施する。
+                    else
+                        Send();
+                }));
+        }
+
+        /// <summary>
+        /// 16Byteごとに改行
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private string FormatData(IEnumerable<byte> data)
+        {
+            var ret = string.Empty;
+            foreach (var (v, i) in data.Select((v, i) => (v, i)))
+            {
+                ret += $"{v:X2} ";
+                if ((i + 1) % 16 == 0) ret += "\n";
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// COMポートOPEN
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            if (ComPort.Text == "") return;
+            if (_serial.Open(ComPort.Text))
+            {
+                ComPort.IsEnabled = false;
+                ComOpen.IsEnabled = false;
+                ComClose.IsEnabled = true;
+            }
+        }
+
+        /// <summary>
+        /// COMポートClose
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            if (_serial.Close())
+            {
+                ComPort.IsEnabled = true;
+                ComOpen.IsEnabled = true;
+                ComClose.IsEnabled = false;
+            }
+        }
+
+        /// <summary>
+        /// ウィンドウ終了
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            _serial.Close();
+        }
+
+        /// <summary>
+        /// ワンショット有効
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OneShot_Checked(object sender, RoutedEventArgs e)
+        {
+            Title = "送信 ⇒ 受信";
+            ComSend.IsEnabled = true;
+        }
+
+        /// <summary>
+        /// ワンショット無効
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OneShot_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Title = "受信 ⇒ 送信";
+            ComSend.IsEnabled = false;
         }
     }
 }
---
+++ SerialPortReceiver/Serial.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+
+namespace SerialPortReceiver
+{
+    internal class Serial
+    {
+        private readonly SerialPort _serialPort = new SerialPort();
+        public IEnumerable<string> PortNames => SerialPort.GetPortNames();
+
+        /// <summary>
+        /// COMポートオープン
+        /// </summary>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public bool Open(string port)
+        {
+            _serialPort.PortName = port;
+            try
+            {
+                _serialPort.ReadTimeout = 10000;
+                _serialPort.WriteTimeout = 10000;
+                _serialPort.Open();
+                _serialPort.DiscardInBuffer();
+                _serialPort.DiscardOutBuffer();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// COMポートクローズ
+        /// </summary>
+        /// <returns></returns>
+        public bool Close()
+        {
+            try
+            {
+                _serialPort.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 受信イベント登録
+        /// </summary>
+        /// <param name="OnReceived"></param>
+        public void SetReceiveEvent(SerialDataReceivedEventHandler OnReceived)
+        {
+            _serialPort.DataReceived += OnReceived;
+        }
+
+        #region Byte
+        /// <summary>
+        /// Byte送信(ASCII)
+        /// </summary>
+        /// <param name="src"></param>
+        public void Send(IEnumerable<byte> src)
+        {
+            if (!_serialPort.IsOpen) return;
+            try
+            {
+                // STX, ETX付与
+                var data = new byte[] { 0x02 }
+                    .Concat(src)
+                    .Concat(new byte[] { 0x03 });
+                _serialPort.Write(data.ToArray(), 0, data.Count());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Byte受信(ASCII)
+        /// </summary>
+        /// <returns></returns>
+        public List<byte> Receive()
+        {
+            var data = new List<byte>();
+            if (!_serialPort.IsOpen) return data;
+
+            var isStx = false;
+            // 受信データ取得
+            while (true)
+            {
+                try
+                {
+                    var d = (byte)_serialPort.ReadByte();
+                    // STXが来るまでは、バッファを読み飛ばす
+                    if (d == 0x02)
+                        isStx = true;
+                    else if (isStx)
+                        // ETXが来たら終了
+                        if (d == 0x03)
+                            break;
+                        else
+                            data.Add(d);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return new List<byte>();
+                }
+            }
+
+            // ASCII → byte変換
+            var ret = new List<byte>();
+            if (data.Count >= 1)
+                for (var i = 0; i < data.Count(); i += 2)
+                {
+                    var tmp = new byte[] { data[i], data[i + 1] };
+                    var tmp2 = Encoding.ASCII.GetString(tmp);
+                    var tmp3 = byte.Parse(tmp2, NumberStyles.HexNumber);
+                    ret.Add(tmp3);
+                }
+            return ret;
+        }
+        #endregion
+
+
+        #region String
+        /// <summary>
+        /// 文字列送信
+        /// </summary>
+        /// <param name="data"></param>
+        public void SendString(string data)
+        {
+            if (!_serialPort.IsOpen) return;
+            try
+            {
+                _serialPort.WriteLine(data);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 文字列受信
+        /// </summary>
+        /// <returns></returns>
+        public string ReceiveString()
+        {
+            var ret = string.Empty;
+            if (!_serialPort.IsOpen) return ret;
+            try
+            {
+                ret = _serialPort.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return ret;
+        }
+        #endregion
+
+
+    }
+}
